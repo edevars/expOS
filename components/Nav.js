a761932b49715b5304e547948d203f25d98acb22
edevars <e.devars.dev@gmail.com>
2019-09-07 12:52:01 -0500

    :sparkles: link effects

--- components/Nav.js
+++ components/Nav.js
@@ -7,18 +7,95 @@ const StyledLink = styled.a`
   height: fit-content;
   padding: 10px;
   font-size: 1.1rem;
+  font-weight: bold;
   &:hover {
     cursor: pointer;
   }
-`
+  *:after,
+  *::before {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  &:hover {
+    cursor: pointer;
+    outline: none;
+  }
+  &::after,
+  &::before {
+    display: inline-block;
+    opacity: 0;
+    -webkit-transition: -webkit-transform 0.3s, opacity 0.2s;
+    -moz-transition: -moz-transform 0.3s, opacity 0.2s;
+    transition: transform 0.3s, opacity 0.2s;
+  }
+
+  &::before {
+    margin-right: 10px;
+    content: "[";
+    -webkit-transform: translateX(20px);
+    -moz-transform: translateX(20px);
+    transform: translateX(20px);
+  }
+
+  &::after {
+    margin-left: 10px;
+    content: "]";
+    -webkit-transform: translateX(-20px);
+    -moz-transform: translateX(-20px);
+    transform: translateX(-20px);
+  }
+
+  &:hover::before,
+  &:hover::after,
+  &:focus::before,
+  &:focus::after {
+    opacity: 1;
+    -webkit-transform: translateX(0px);
+    -moz-transform: translateX(0px);
+    transform: translateX(0px);
+  }
+`;
 
 const StyledNav = styled.nav`
   background: ${props => props.theme.color1}; /* fallback for old browsers */
   background: ${props => props.theme.gradient};
-  height:  80px;
+  height: 80px;
   display: flex;
   justify-content: flex-end;
   align-items: center;
+`;
+
+const Registrarse = styled.button`
+  background-color: ${props => props.theme.color2};
+  color: ${props => props.theme.light};
+  border: none;
+  height: 30px;
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 1.05rem;
+  font-weight: bold;
+  outline: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const IniciarSesion = styled.button`
+  background-color: ${props => props.theme.color3};
+  color: ${props => props.theme.light};
+  border: none;
+  height: 30px;
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 1.05rem;
+  font-weight: bold;
+  outline: none;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Nav = () => (
@@ -32,9 +109,8 @@ const Nav = () => (
     <Link href="/workshops">
       <StyledLink>workshops</StyledLink>
     </Link>
-    <Link href="/registro">
-      <StyledLink>Ingresa</StyledLink>
-    </Link>
+    <Registrarse>Registrarse</Registrarse>
+    <IniciarSesion>Iniciar Sesion</IniciarSesion>
   </StyledNav>
 );
 
