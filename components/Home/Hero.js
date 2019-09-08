81da8dfa46f3bf8c4c081ad817e283a584784b74
edevars <e.devars.dev@gmail.com>
2019-09-07 23:25:50 -0500

    :sparkles: New Counter date time

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -5,13 +5,12 @@ import logo from "../../images/logo.png";
 
 const StyledHero = styled.div`
   width: 100vw;
-  height: 60vh;
+  height: 80vh;
   position: relative;
   background-position: center;
   background-size: cover;
   display: flex;
   justify-content: center;
-  align-items: center;
   .myVideo {
     position: absolute;
     right: 0;
@@ -28,14 +27,23 @@ const StyledHero = styled.div`
 
 const Logo = styled.img`
   position: absolute;
-  width: 60%;
+  width: 40%;
   height: auto;
   z-index: 2;
+  margin-top: 20vh;
+`;
+
+const Counter = styled.h1`
+  color: ${props => props.theme.light};
+  font-size: 46px;
+  position: absolute;
+  z-index: 2;
+  margin-top: 40vh;
 `;
 
 const Mask = styled.div`
   position: absolute;
-  background: ${props => props.theme.color1}; 
+  background: ${props => props.theme.color1};
   width: 100%;
   height: 100%;
   opacity: 0.3;
@@ -47,9 +55,32 @@ class Hero extends Component {
     super();
   }
 
+  state = {
+    finalDate: new Date("Sep 18, 2019 00:00:00").getTime(),
+    now: new Date().getTime()
+  };
+
   componentDidMount() {
     this.refs.vidRef.play();
+    setInterval(() => {
+      this.setState({
+        finalDate: new Date("Sep 18, 2019 00:00:00").getTime(),
+        now: new Date().getTime()
+      });
+    }, 1000);
   }
+
+  calculateDate = () => {
+    let distance = this.state.finalDate - this.state.now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    return "Solo faltan " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  };
 
   render() {
     return (
@@ -59,6 +90,7 @@ class Hero extends Component {
         </video>
         <Logo src={logo}></Logo>
         <Mask></Mask>
+        <Counter>{this.calculateDate()}</Counter>
       </StyledHero>
     );
   }
