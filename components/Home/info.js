637f717a2cf7e7123b6f91fb8054df4e6fb710ed
edevars <e.devars.dev@gmail.com>
2019-09-07 22:53:09 -0500

    :sparkles: preliminar info container

---
+++ components/Home/info.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    background: ${props => props.theme.light};
+    width: 900px;
+    height: 170px;
+    position: absolute;
+    bottom: -85px;
+    z-index: 2;
+`;
+
+
+const InfoContainer = () => (
+    <Wrapper>
+       
+    </Wrapper>
+)
+
+export default InfoContainer
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import React from "react";
-import styled from "styled-components";
 import Hero from "../components/Home/Hero";
+import Info from "../components/Home/info";
+import styled from "styled-components";
+
+const HeroWrapper = styled.div`
+  position: relative;
+  z-index: 0;
+  height: auto;
+  display: flex;
+  justify-content: center;
+`;
 
 const Home = () => (
   <>
-    <Hero></Hero>
+    <HeroWrapper>
+      <Hero></Hero>
+      <Info></Info>
+    </HeroWrapper>
   </>
 );
 
