abeba8a785eef66f0d88d834dbdcc1b54f179958
edevars <e.devars.dev@gmail.com>
2019-09-11 23:13:18 -0500

    :sparkles: Responsive Design Wokrshops

--- components/speaker.js
+++ components/speaker.js
@@ -4,13 +4,16 @@ const SpeakerWrapper = styled.div`
   background-color: ${props => props.theme.light_dimmed};
   display: grid;
   max-width: 600px;
-  grid-template: minmax(300px, 1fr) / 250px auto;
+  grid-template: 1fr / 250px auto;
   grid-template-areas: "avatar info";
-  margin-top: 60px;
-  margin-right: 80px;
   -webkit-box-shadow: 0px 4px 26px 3px rgba(199, 199, 199, 1);
   -moz-box-shadow: 0px 4px 26px 3px rgba(199, 199, 199, 1);
   box-shadow: 0px 4px 26px 3px rgba(199, 199, 199, 1);
+  padding: 0px 25px;
+  @media screen and (max-width: 768px) {
+    grid-template: 250px 1fr / auto;
+    grid-template-areas: "avatar" "info";
+  }
 `;
 
 const Profile = styled.img`
@@ -19,15 +22,26 @@ const Profile = styled.img`
   height: 200px;
   margin: auto auto;
   border-radius: 50%;
+  object-fit: cover;
 `;
 const Info = styled.div`
   grid-area: info;
   width: 100%;
+  .date {
+    margin-top: 0;
+    font-weight: bold;
+    font-size: 0.8rem;
+  }
+  .name {
+    color: ${props => props.theme.color3};
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
 `;
 
 const Title = styled.h3`
   color: ${props => props.theme.color1};
-  font-size: 2rem;
+  font-size: 1.85rem;
   margin-bottom: 0;
 `;
 
@@ -44,24 +58,43 @@ const Link = styled.a`
   color: ${props => props.theme.light};
   padding: 5px 10px;
   text-align: center;
-  margin-bottom: 20px;
+
+  margin-bottom: 40px;
   &:hover {
     cursor: pointer;
   }
+
+  @media screen and (max-width: 425px) {
+    margin: 0 auto;
+    margin-bottom: 40px;
+    width: calc(100% - 20px);
+  }
+`;
+const ImageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Speaker = props => {
   return (
     <SpeakerWrapper>
-      <Profile
-        src={
-          props.image ||
-          "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/7_avatar-512.png"
-        }
-      ></Profile>
+      <ImageWrapper>
+        <Profile
+          src={
+            props.image ||
+            "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/7_avatar-512.png"
+          }
+        ></Profile>
+      </ImageWrapper>
       <Info>
         <Title>{props.title || "Titulo"}</Title>
-        <p style={{fontWeight: "bold", fontSize: ".8rem"}}>{props.time || "00:00hrs"} | {props.date || "18 de septiembre 2019"}</p>
+        <p className="name">{props.name || "Nombre Speaker"}</p>
+        <p className="date">
+          {props.time || "00:00hrs"} | {props.date || "18 de septiembre 2019"}
+        </p>
         <Description>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam
           laboriosam incidunt deserunt animi alias beatae ullam ducimus. Quas
